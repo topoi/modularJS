fab8765cd988687e7b4073966255ae958889e0eb
gordon <gordon@gordon-HP-Z620-Workstation.(none)>
2013-06-03 09:28:11 +0200

    callMaterial

---
+++ CallMaterial.js
@@ -0,0 +1,18 @@
+var material="None";
+
+function callMaterial(material,d)
+{
+    
+alert("call material: "+material);
+var svg = d3.select("#callMaterial").append("svg:svg");
+activateMat="true";
+tags.filter(function(d){
+tag=d.indexOf(material);
+alert("tag call material: "+tag);
+filterMaterial(material,tag)
+{
+filtMat[d[0]]=document.getElementById('filterMaterial').innerHTML;
+};
+}); 
+start();
+};
---
+++ CallTyp.js
@@ -0,0 +1,14 @@
+var typ="None";
+function callTyp(typ,d)
+{
+var svg = d3.select("#callTyp").append("svg:svg");
+activateTyp="true";
+tags.filter(function(d){
+tag=d.indexOf(typ);
+filterTyp(typ,tag)
+{
+filtTyp[d[0]]=document.getElementById('filterTyp').innerHTML;
+};
+}); 
+start();
+};
