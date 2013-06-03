5b6f8775c79e4835dbe8b0a61f7d0221ab0a7aed
gordon <gordon@gordon-HP-Z620-Workstation.(none)>
2013-06-03 09:07:15 +0200

    first commit

---
+++ CallDatum.js
@@ -0,0 +1,15 @@
+
+var datum="None";
+function callDatum(datum,d)
+{
+var svg = d3.select("#callDatum").append("svg:svg");
+activateDat="true";
+tags.filter(function(d){
+tag=d.indexOf(datum);
+filterDatum(datum,tag)
+{
+filtDat[d[0]]=document.getElementById('filterDatum').innerHTML;
+};
+}); 
+start();
+};
