5ddbd5905f4d6ac21af597859db328cc5c824570
grobian <gordon-fischer@gmx.de>
2013-06-04 15:53:12 +0200

    Update CallMaterial.js

--- CallMaterial.js
+++ CallMaterial.js
@@ -5,12 +5,15 @@ function callMaterial(material,d)
     
 var svg = d3.select("#callMaterial").append("svg:svg");
 activateMat="true";
-tags.filter(function(d){
-tag=d.indexOf(material);
+
 filterMaterial(material,tag)
 {
-filtMat[d[0]]=document.getElementById('filterMaterial').innerHTML;
-};
+ 
+tags.filter(function(d){
+tag=d.indexOf(material);  
+    filtMat[d[0]]=document.getElementById('filterMaterial').innerHTML;
 }); 
+};
+
 start();
 };
