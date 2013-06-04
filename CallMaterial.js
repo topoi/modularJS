d1b8253907ad70c4d5e8651e7b71259d76eea03f
grobian <gordon-fischer@gmx.de>
2013-06-04 16:48:11 +0300

    Update CallMaterial.js
    alerts entfernt

--- CallMaterial.js
+++ CallMaterial.js
@@ -3,12 +3,10 @@ var material="None";
 function callMaterial(material,d)
 {
     
-alert("call material: "+material);
 var svg = d3.select("#callMaterial").append("svg:svg");
 activateMat="true";
 tags.filter(function(d){
 tag=d.indexOf(material);
-alert("tag call material: "+tag);
 filterMaterial(material,tag)
 {
 filtMat[d[0]]=document.getElementById('filterMaterial').innerHTML;
